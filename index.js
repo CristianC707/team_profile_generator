e567e30139c737cef4348d8b5299446c50d49936
CristianC707 <cristian.e.cuevas707@gmail.com>
2021-07-19 22:15:55 -0400

    Created classes for generator

---
+++ index.js
@@ -0,0 +1,22 @@
+// require classes
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern');
+// require our packages
+
+
+
+
+
+menu = () => {
+    //create seperate functions for each class creation
+}
+
+
+
+
+
+
+
+menu();
